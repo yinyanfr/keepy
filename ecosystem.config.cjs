1862673d24ec30e5032cb32bd7bcf2cfb1b632fd
Yan <i@yinyan.fr>
2026-06-09 22:50:22 +0800

    Add PM2 serve config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "keepy",
+      script: "dist/app.js",
+      cwd: __dirname,
+      exec_mode: "fork",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "300M",
+      env: {
+        NODE_ENV: "production",
+      },
+      error_file: "logs/pm2-error.log",
+      out_file: "logs/pm2-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    },
+  ],
+};
